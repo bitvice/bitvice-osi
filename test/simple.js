852c69587b04d798dc60b6088d5025b680231c8f
Gabriel Balasz <gabi@bitvice.ro>
2015-07-03 12:35:50 +0300

    Reached complete test code coverage

--- test/simple.js
+++ test/simple.js
@@ -24,110 +24,159 @@ describe('OSI', function(){
         });
     });
 
-    it('Should set the right value', function () {
-        var response;
-        var expected = 100;
+    describe('Consumers before, serivces after', function(){
+        it('Consumers are waiting for services to become available', function () {
+            var response;
+            var expected = 100;
 
-        // Define the consumer first
+            // Define the consumer first
 
-        var testDependencies = ['basicService', 'secondService']
+            var testDependencies = ['basicService', 'secondService'];
 
-        function TestConsumer () {
-            this._requires = testDependencies;
-            OSI.ServiceConsumer.call(this);
-        }
+            function TestConsumer () {
+                this._requires = testDependencies;
+                OSI.ServiceConsumer.call(this);
+            }
 
-        util.inherits(TestConsumer, OSI.ServiceConsumer);
+            util.inherits(TestConsumer, OSI.ServiceConsumer);
 
-        TestConsumer.prototype.__constructor = function () {
-            var basicService  = this.service('basicService' );
-            var secondService = this.service('secondService');
+            TestConsumer.prototype.__constructor = function () {
+                var basicService  = this.service('basicService' );
+                var secondService = this.service('secondService');
 
-            response = basicService.run() * secondService.run();
-        };
+                response = basicService.run() * secondService.run();
+            };
 
-        var consumerInstance = new TestConsumer();
+            var consumerInstance = new TestConsumer();
 
-        // test
-        should.exist(consumerInstance);
-
-
-
-        function Nonsense () {
-            OSI.ServiceConsumer.call(this);            
-        }
-
-        util.inherits(Nonsense, OSI.ServiceConsumer);
-
-        var nonsenseInstance = new Nonsense();
-
-        // test
-        should.exist(nonsenseInstance);
-
-        // test
-        should.exist(nonsenseInstance.__constructor);
-
-        // test
-        should.not.exist(nonsenseInstance.__constructor());
+            // test
+            should.exist(consumerInstance);
 
 
 
+            function Nonsense () {
+                OSI.ServiceConsumer.call(this);            
+            }
 
-        // Define the basic service here
+            util.inherits(Nonsense, OSI.ServiceConsumer);
 
-        function BasicService () {
-            OSI.Service.call(this);
-        }
+            var nonsenseInstance = new Nonsense();
 
-        util.inherits(BasicService, OSI.Service);
+            // test
+            should.exist(nonsenseInstance);
 
-        BasicService.prototype.run = function () {
-            return 25;
-        };
+            // test
+            should.exist(nonsenseInstance.__constructor);
 
-        OSI.ServiceProvider.registerService('basicService', BasicService);
-
-
-        // test
-        should.not.exist(response);
+            // test
+            should.not.exist(nonsenseInstance.__constructor());
 
 
 
-        // Define the second service here
 
-        function SecondService () {
-            OSI.Service.call(this);
-        }
+            // Define the basic service here
 
-        util.inherits(SecondService, OSI.Service);
+            function BasicService () {
+                OSI.Service.call(this);
+            }
 
-        SecondService.prototype.run = function () {
-            return 4;
-        };
+            util.inherits(BasicService, OSI.Service);
 
-        OSI.ServiceProvider.registerService('secondService', SecondService);
+            BasicService.prototype.run = function () {
+                return 25;
+            };
 
-
-
-        function NonsenseService () {
-            OSI.Service.call(this);            
-        }
-
-        util.inherits(NonsenseService, OSI.Service);
-
-        OSI.ServiceProvider.registerService('nonsenseService', NonsenseService);
-
-        var ns = OSI.ServiceProvider.service('nonsenseService');
-
-        // test
-        expect(ns.run).to.be.a('function');
-
-        // test
-        should.not.exist(ns.run());
-        
+            OSI.ServiceProvider.registerService('basicService', BasicService);
 
 
-        // test
-        expect(response).to.be.a('number', 100);
+            // test
+            should.not.exist(response);
+
+
+
+            // Define the second service here
+
+            function SecondService () {
+                OSI.Service.call(this);
+            }
+
+            util.inherits(SecondService, OSI.Service);
+
+            SecondService.prototype.run = function () {
+                return 4;
+            };
+
+            OSI.ServiceProvider.registerService('secondService', SecondService);
+
+
+
+            function NonsenseService () {
+                OSI.Service.call(this);            
+            }
+
+            util.inherits(NonsenseService, OSI.Service);
+
+            OSI.ServiceProvider.registerService('nonsenseService', NonsenseService);
+
+            var ns = OSI.ServiceProvider.service('nonsenseService');
+
+            // test
+            expect(ns.run).to.be.a('function');
+
+            // test
+            should.not.exist(ns.run());
+            
+
+
+            // test
+            expect(response).to.be.a('number', expected);
+        });
+
+    });
+
+    describe('Services before, Consumers after', function(){
+
+        it('Consumer is runing the existing service', function () {
+
+            var response;
+            var expected = 100;
+            var basicServiceName = 'basicService';
+
+            // Define the basic service here
+
+            function BasicService () {
+                OSI.Service.call(this);
+            }
+
+            util.inherits(BasicService, OSI.Service);
+
+            BasicService.prototype.run = function () {
+                response = expected;
+            };
+
+            OSI.ServiceProvider.registerService(basicServiceName, BasicService);
+
+
+
+            // Consumer
+
+            var testDependencies = [basicServiceName];
+
+            function TestConsumer () {
+                this._requires = testDependencies;
+                OSI.ServiceConsumer.call(this);
+            }
+
+            util.inherits(TestConsumer, OSI.ServiceConsumer);
+
+            var consumerInstance = new TestConsumer();
+
+            // test
+            should.exist(consumerInstance);
+
+
+            // test
+            expect(response).to.be.a('number', expected);
+        });
     });
 });
