b061f22890d57a30fb27b1735943241bab0045b9
Gabriel Balasz <gabi@bitvice.ro>
2015-07-07 01:52:25 +0300

    Removed old service-base

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//
+// OSI
+//
+// Observer Services Injection
+//
+// Copyright(c) 2015 Gabriel Balasz <gabi@bitvice.ro>
+// MIT Licensed
+//
+
 'use strict';
 
 var consumer = require('./lib/service-consumer');
@@ -7,7 +16,6 @@ var AService  = require('./lib/abstracts/service.abstract');
 var osi = module.exports;
 
 osi.ServiceConsumer = consumer ;
-osi.ServiceProvider = provider ;
 osi.AService        = AService  ;
 
 // TODO: Move ServiceProvider here
