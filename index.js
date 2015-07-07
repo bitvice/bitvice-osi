45ea164742edcb12b7d94ee7c94c1d9f7357444f
Gabriel Balasz <gabi@bitvice.ro>
2015-07-07 14:18:10 +0300

    Use promises to request services
    Got rid of lodash dependency.

--- index.js
+++ index.js
@@ -18,45 +18,58 @@ osi.AService  = require('./lib/abstracts/service.abstract');
 
 // TODO: Can we use promises ? Think so :)
 
+
+
+
 var _ = require('lodash');
 
 var exports = module.exports;
 
 // Stores the list of registered services
-var servicesCache = {};
+var servicesCache = new WeakSet;
 
 // Stores the list of classes that waits for services to be registered
-var dependencies = {};
+var dependencies = new WeakMap;
+
+function serviceObject (serviceName) {
+    return {servoce: serviceName};
+}
 
 //
 // Register a class (who) to be notified when a specific service (what) will be available
 //
-osi.requestService = function (who, what) {
-    if (!_.isUndefined(servicesCache[what])) {
-        who.injectService(servicesCache[what]);
+osi.requestService = function (serviceName, fnResolve, fnReject) {
+
+    var objService = serviceObject(serviceName);
+
+    if (servicesCache.has(serviceName)) {
+        fnResolve(servicesCache.get(serviceName));
         return;
     }
 
-    if (!_.has(dependencies, what)) {
-        dependencies[what] = [];
+    if (!dependencies.has(serviceName) === "undefined") {
+        dependencies.add(objService, new Set);
     }
 
-    dependencies[what].push(who);
+    dependencies.get(objService) . 
+        add({resolve:fnResolve, reject: fnReject});
 };
 
 //
 // Receive a service and notify the consumers about it's existence
 //
 osi.registerService = function (serviceName, serviceClass) {
+    var objService = serviceObject(serviceName);
     var serviceInstance = new serviceClass();
     serviceInstance.setName(serviceName);
 
-    servicesCache[serviceName] = serviceInstance;
+    servicesCache.set(objService, serviceInstance);
 
-    if (_.has(dependencies, serviceName)) {
-        _.each(dependencies[serviceName], function (consumer) {
-            consumer.injectService(serviceInstance);
-        });
+    if (dependencies.has(objService)) {
+        dependencies.get(objService) . 
+            each (function (consumer) {
+                consumer.resolve(serviceInstance);
+            });
     }
 };
 
@@ -64,5 +77,5 @@ osi.registerService = function (serviceName, serviceClass) {
 // Retrieves the service instance for a spcific service name, it it exists
 //
 osi.service = function (serviceName) {
-    return servicesCache[serviceName];
+    return servicesCache.get(serviceObject(serviceName));
 };
