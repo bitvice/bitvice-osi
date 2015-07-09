e9e01e9e17ce88839c46c83fa1c0b891e9125f33
Gabriel Balasz <gabi@bitvice.ro>
2015-07-09 14:59:09 +0300

    Added comments to main file

--- index.js
+++ index.js
@@ -7,10 +7,14 @@
 // MIT Licensed
 //
 
+// Load OSI methods
 var OSI = require('./lib/osi.mod');
 
+// Define the consumer abstract class
 OSI.AConsumer = require('./lib/abstracts/consumer.abstract');
 
+// Define the service abstract class
 OSI.AService  = require('./lib/abstracts/service.abstract');
 
+// Export OSI object
 module.exports = OSI;
